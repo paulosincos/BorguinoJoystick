d5c4d36ad44c7e9f919aa7afc6b8463d5f1ea3df
Paulo César Botelho Sincos <paulosincos@hotmail.com>
2026-07-15 00:20:07 -0300

    feat: add samples to main

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,26 @@ Joystick_ joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
 
 
 DigitalPinInput pinD2Input(2);
-AnalogPinInput analogInput(A0);
-SignalInput* inputs[] = {&pinD2Input, &analogInput};
+AnalogPinInput pinA0Input(A0);
+AnalogPinInput pinA1Input(A1);
+AnalogPinInput pinA2Input(A2);
+AnalogPinInput pinA3Input(A3);
+UpdatableSignalInput* inputs[] = {&pinD2Input, &pinA0Input, &pinA1Input, &pinA2Input, &pinA3Input};
+
+ComposedAnalogPinInput pinA2A3Input(pinA2Input, pinA3Input);
 
 JoystickButtonOutput joystickButtonOutput(joystick, 0, pinD2Input);
-SignalOutput* outputs[] = {&joystickButtonOutput};
+JoystickXAxisOutput joystickXAxisOutput(joystick, pinA0Input);
+JoystickYAxisOutput joystickYAxisOutput(joystick, pinA1Input);
+JoystickZAxisOutput joystickZAxisOutput(joystick, pinA2A3Input);
+SignalOutput* outputs[] = {&joystickButtonOutput, &joystickXAxisOutput, &joystickYAxisOutput, &joystickZAxisOutput};
 
 void setup() {
   joystick.begin();
 }
 
 void loop() {
-  for (SignalInput* input : inputs) {
+  for (UpdatableSignalInput* input : inputs) {
     input->update();
   }
 
