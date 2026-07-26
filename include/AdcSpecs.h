ac8484dce48909d53ff73db64205e26d51c827f9
Paulo César Botelho Sincos <paulosincos@hotmail.com>
2026-07-26 13:18:03 -0300

    chore: add adc specs doc

--- include/AdcSpecs.h
+++ include/AdcSpecs.h
@@ -1,8 +1,11 @@
 #ifndef ADCSPECS_H
 #define ADCSPECS_H
 
+// The minimum value for a 10-bit ADC (Analog-to-Digital Converter)
 constexpr int ADC_MIN_VALUE = 0;
+// The maximum value for a 10-bit ADC (Analog-to-Digital Converter)
 constexpr int ADC_MAX_VALUE = 1023;
+// The center value for a 10-bit ADC (Analog-to-Digital Converter)
 constexpr int ADC_CENTER_VALUE = (ADC_MIN_VALUE + ADC_MAX_VALUE) / 2;
 
 #endif
